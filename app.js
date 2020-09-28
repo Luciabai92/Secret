eae46015d45fc0440cf235ea533f5915ee07ecee
luciabai92 <bailu19920907@sina.com>
2020-09-28 14:47:14 -0700

    md5&bcrypt-install

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //jshint esversion:6
 //dotenv
-require('dotenv').config()
+//require('dotenv').config()
 //express
 const express = require("express");
 const app = express();
@@ -15,7 +15,7 @@ app.set('view engine', 'ejs');
 
 //mongoose
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+//const encrypt = require("mongoose-encryption");
 mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -26,7 +26,10 @@ const userSchema = new mongoose.Schema ({
   password:String
 });
 
-userSchema.plugin(encrypt,{secret: process.env.SECRET, encryptedFields:["password"]});
+//userSchema.plugin(encrypt,{secret: process.env.SECRET, encryptedFields:["password"]});
+
+//md5
+const md5 = require("md5");
 
 const User = new mongoose.model("User",userSchema);
 
@@ -49,7 +52,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   })
 
   newUser.save(function(err){
@@ -63,7 +66,7 @@ app.post("/register",function(req,res){
 
 app.post("/login",function(req,res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email:username}, function(err,foundUser){
     if(err) {
